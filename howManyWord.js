17f21c5fb213eee6816162cfd47b577c30b9606b
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-03-26 21:32:22 -0300

    Added "How Many Word?"

---
+++ howManyWord.js
@@ -0,0 +1,11 @@
+/*
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+*/
+
+function testit(s){
+  return (s.match(/w.*?o.*?r.*?d.*?/gi) || []).length
+}
