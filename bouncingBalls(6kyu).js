07ccc4d33826b1f31bfd53ba462225dd8d99646a
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-03-30 07:16:36 -0300

    Added solution

--- bouncingBalls(6kyu).js
+++ bouncingBalls(6kyu).js
@@ -23,3 +23,13 @@
 
 	(Condition 2) not fulfilled).
 */
+
+function bouncingBall(h,  bounce,  window) {
+  if(h < 0 || bounce < 0 || bounce >= 1 || window >= h) return -1
+  let counter = 0;
+  while(h > window){
+    h *= bounce;
+    counter++
+  }
+  return counter * 2 - 1
+}
