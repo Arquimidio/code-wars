f1d530b753f5d98c85b4e29ec4e9e97d63966c00
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-04-01 13:15:45 -0300

    Add "IP Validation"

---
+++ ipValidation.js
@@ -0,0 +1,28 @@
+/*
+ Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+
+ Valid inputs examples:
+ Examples of valid inputs:
+ 1.2.3.4
+ 123.45.67.89
+ Invalid input examples:
+ 1.2.3
+ 1.2.3.4.5
+ 123.456.78.90
+ 123.045.067.089
+ Notes:
+ Leading zeros (e.g. 01.02.03.04) are considered invalid
+ Inputs are guaranteed to be a single string
+*/
+
+function isValidIP(str) {
+  let divided = str.split('.')
+  if(divided.length !== 4) return false
+  return divided.every(num => {
+    let string = num
+    let number = Number(num)
+    let isBetween0and255 = 0 <= number && number <= 255
+    let hasOnlyDigits = parseInt(num).toString() === string
+    return isBetween0and255 && hasOnlyDigits
+  });
+}
