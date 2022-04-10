8f50853d841f3afef81645fda25715dd81c7153f
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-04-09 23:15:33 -0300

    Add "Sudoku Validator"

---
+++ sudokuValidator(refatorar).js
@@ -0,0 +1,57 @@
+/*
+Sudoku Background
+Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9.
+(More info at: http://en.wikipedia.org/wiki/Sudoku)
+
+Sudoku Solution Validator
+Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+
+The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+
+Examples
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]); // => true
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+  [6, 7, 2, 1, 9, 0, 3, 4, 8],
+  [1, 0, 0, 3, 4, 2, 5, 6, 0],
+  [8, 5, 9, 7, 6, 1, 0, 2, 0],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 0, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]); // => false
+
+*/
+
+function validSolution(board){
+  let isValid = true;
+  for(let i = 0; i < board.length; i++){
+    const col = board.map(row => row[i])
+    const noDupCols = col.length === new Set(col).size
+    if(!noDupCols){
+      isValid = false;
+    }
+  }
+  if(!board.every(row => row.length === new Set(row).size)) isValid = false
+  for(let i = 0; i < 3; i++){
+    const threeRows = board.slice(i * 3, i * 3 + 3)
+    for(let j = 0; j < 3; j++){
+      let arr = [].concat(...threeRows.map(row => row.slice(j * 3, j * 3 + 3)))
+      if(arr.length !== new Set(arr).size){
+        isValid = false;
+      }
+    }
+  }
+  return isValid;
+}
