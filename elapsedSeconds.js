c501098e80f7806ece6c00678504407fa4ca0bd6
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-04-16 16:13:03 -0300

    Add "Elapsed Seconds"

---
+++ elapsedSeconds.js
@@ -0,0 +1,13 @@
+/*
+
+Complete the function so that it returns the number of seconds that have elapsed between the start and end times given.
+
+Tips:
+The start/end times are given as Date (JS/CoffeeScript), DateTime (C#), Time (Nim), datetime(Python) and Time (Ruby) instances.
+The start time will always be before the end time.
+
+*/
+
+function elapsedSeconds(startDate, endDate){
+  return (new Date(endDate) - new Date(startDate))/1000
+}
