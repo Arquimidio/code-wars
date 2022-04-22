529a7fc1a4c0acf1ee1b44c1b12c2df30f546b1a
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-04-22 10:36:16 -0300

    Add "Twice Linear"

---
+++ twiceLinear(gottorefactor).js
@@ -0,0 +1,33 @@
+/*
+Consider a sequence u where u is defined as follows:
+
+The number u(0) = 1 is the first one in u.
+For each x in u, then y = 2 * x + 1 and z = 3 * x + 1 must be in u too.
+There are no other numbers in u.
+Ex: u = [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
+
+1 gives 3 and 4, then 3 gives 7 and 10, 4 gives 9 and 13, then 7 gives 15 and 22 and so on...
+
+Task:
+Given parameter n the function dbl_linear (or dblLinear...) returns the element u(n) of the ordered (with <) sequence u (so, there are no duplicates).
+
+Example:
+dbl_linear(10) should return 22
+
+Note:
+Focus attention on efficiency
+*/
+
+function dblLinear(n) {
+  let arr = [1]
+  let counter = 0;
+  while(arr.length < n * 10){
+    const x = arr[counter];
+    const y = 2 * x + 1;
+    const z = 3 * x + 1;
+    arr.push(y, z);
+    counter++
+  }
+  arr.sort((a, b) => a - b);
+  return [...new Set(arr)][n];
+}
