dc1d08df01a40da9db2ecb7f53f12c5a5b62b925
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-04-27 09:43:38 -0300

    Add "Playing With Sets: Equal or Not"

--- pwsEqual||not.js
+++ pwsEqual||not.js
@@ -1,5 +1,5 @@
 /*
-
+Create 2 functions, areEqual and notEqual, getting 2 sets as arguments and returning a true or false depending if these 2 sets are "equal" (respectively not equal) ie : if all elements of 1st set are elements of 2d and all elements of 2d set are elements of 1st.
 */
 
 function areEqual(s1, s2){
