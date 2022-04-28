4594ddaf335c9957f46b667d72ac5092f8f63237
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-04-28 10:32:58 -0300

    Add "Fun with classes: people, people, people"

--- circleInsideSquare.js
+++ circleInsideSquare.js
@@ -7,8 +7,6 @@ You get the blue+red area and need to calculate the red area.
 Your function gets a number which represents the area of the square and should return the area of the circle. The tests are rounded by 8 decimals to make sure multiple types of solutions work.
 
 You don't have to worry about error handling or negative number input: area >= 0.
-
-
 */
 
 function squareAreaToCircle(size){
