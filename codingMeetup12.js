5f9e64d19c950636d0f796c3dd6957d751fd423c
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-01 13:19:53 -0300

    Add "Coding Meetup 12"

---
+++ codingMeetup12.js
@@ -0,0 +1,16 @@
+/*
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+];
+
+write a function that when executed as findAdmin(list1, 'JavaScript') returns only the JavaScript developers who are GitHub admins:
+*/
+
+function findAdmin(list, lang) {
+  return list.filter(dev => dev.githubAdmin === 'yes' && dev.language === lang);
+}
