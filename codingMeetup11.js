8e7d9aee8e655f44f690dcd3aedc9cd73af7ddd0
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-01 13:18:52 -0300

    Add "Coding Meetup 11"

---
+++ codingMeetup11.js
@@ -0,0 +1,16 @@
+/*
+var list1 = [
+  { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+];
+write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer.
+*/
+
+function getAverageAge(list) {
+  return Math.round(list.reduce((acc, cur) => acc + cur.age, 0) / list.length)
+}
