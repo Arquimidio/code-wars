58bcb3208aba41b3618c212754a198aaaac1d1e5
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-02 11:02:59 -0300

    Add "You cant code under pressure"

---
+++ cantCodeUnderPressure.js
@@ -0,0 +1,11 @@
+/*
+Code as fast as you can!
+
+You need to make a constructor function with two methods (and only these two methods) to return and increment a counter, but the counter should not be directly accessible from outside the function.
+*/
+
+function Counter() {
+  let counter = 0;
+  this.check = () => counter;
+  this.increment = () => counter++;
+};
