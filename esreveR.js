7ff8342bcdadf787a2234c9d948a9135f0c30e60
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-04 10:51:45 -0300

    Add "esreveR"

---
+++ esreveR.js
@@ -0,0 +1,9 @@
+/*
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+*/
+
+reverse = function(array) {
+  return array.map((elt, i) => array[array.length - i - 1])
+}
