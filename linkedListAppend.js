11140e7d61efa952632da8c5655ea16c3dc546a2
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-05 10:23:54 -0300

    Add "Linked List Append"

---
+++ linkedListAppend.js
@@ -0,0 +1,30 @@
+/*
+Linked Lists - Append
+
+Write an Append() function which appends one linked list to another. The head of the resulting list should be returned.
+
+var listA = 1 -> 2 -> 3 -> null
+var listB = 4 -> 5 -> 6 -> null
+append(listA, listB) === 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+If both listA and listB are null/NULL/None/nil, return null/NULL/None/nil. If one list is null/NULL/None/nil and the other is not, simply return the non-null/NULL/None/nil list.
+
+The push() and buildOneTwoThree() (build_one_two_three() in PHP and ruby) functions need not be redefined. The Node class is also predefined for you in PHP.
+
+Related Kata in order of expected completion (increasing difficulty):
+*/
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function append(listA, listB) {
+  if(!listA) return listB;
+  if(!listB) return listA;
+  let tempList = listA;
+  while(tempList.next){
+    tempList = tempList.next;
+  }
+  tempList.next = listB
+  return listA
+}
