fb975fa8164a80809cbe8c1a1510e87807a0cc70
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-13 12:51:12 -0300

    Add "Reverse Every Other"

---
+++ reverseEveryOther.js
@@ -0,0 +1,11 @@
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+
+function reverse(str){
+  return str.split(' ').map((elt, i) =>{
+    const cleaned = elt.trim();
+    if(i % 2 !== 0) return [...cleaned].reverse().join('')
+    return cleaned
+  }).join(' ').trim()
+}
