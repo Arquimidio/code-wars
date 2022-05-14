470669fe43ac9f84e846926b49fc54fd27db6400
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-14 11:03:04 -0300

    Add "Word to Binary"

---
+++ wordToBinary.js
@@ -0,0 +1,10 @@
+/*
+Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. The binary strings should be eight digits long.
+
+Example: 'man' should return [ '01101101', '01100001', '01101110' ]
+
+*/
+
+function wordToBin(str){
+  return [...str].map(char => char.charCodeAt().toString(2).padStart(8, '0'));
+}
