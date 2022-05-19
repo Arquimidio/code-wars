f639e656a48eb2714b19dee6d0b4bcf90ebdaeed
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-19 12:32:31 -0300

    Add "Run Length Encoding"

---
+++ runLengthEncoding.js
@@ -0,0 +1,38 @@
+/*
+Run-length encoding (RLE) is a very simple form of data compression in which runs of data (that is, sequences in which the same data value occurs in many consecutive data elements) are stored as a single data value and count, rather than as the original run. Wikipedia
+
+Task
+Your task is to write such a run-length encoding. For a given string, return a list (or array) of pairs (or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct the original string by replicating the character sx ix times and concatening all those strings. Your run-length encoding should be minimal, ie. for all i the values si and si+1 should differ.
+
+Examples
+As the article states, RLE is a very simple form of data compression. It's only suitable for runs of data, as one can see in the following example:
+
+runLengthEncoding("hello world!")
+ //=>      [[1,'h'], [1,'e'], [2,'l'], [1,'o'], [1,' '], [1,'w'], [1,'o'], [1,'r'], [1,'l'], [1,'d'], [1,'!']]
+It's very effective if the same data value occurs in many consecutive data elements:
+
+runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
+ // => [[34,'a'], [3,'b']]
+*/
+
+// Regex solution
+const runLengthEncoding = function(str){
+  return (str.match(/(.)\1*/g) || []).map(seq => [seq.length, seq[0]])
+}
+
+// Iterative solution
+const runLengthEncoding = function(str){
+  if(!str.length) return [];
+  const result = [[0, str[0]]];
+  let actualChar = str[0];
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === actualChar){
+      result[result.length - 1][0]++;
+    }
+    else{
+      result.push([1, str[i]]);
+      actualChar = str[i];
+    }
+  }
+  return result;
+}
