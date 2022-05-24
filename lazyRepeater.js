b5bf3922f6692042ad38e13c87adb4cef76664b2
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-24 07:08:30 -0300

    Add "Lazy Repeater"

---
+++ lazyRepeater.js
@@ -0,0 +1,23 @@
+/*
+The makeLooper() function (make_looper in Python) takes a string (of non-zero length) as an argument. It returns a function. The function it returns will return successive characters of the string on successive invocations. It will start back at the beginning of the string once it reaches the end.
+
+For example:
+
+var abc = makeLooper('abc');
+abc(); // should return 'a' on this first call
+abc(); // should return 'b' on this second call
+abc(); // should return 'c' on this third call
+abc(); // should return 'a' again on this fourth call
+Func<char> abc = Kata.MakeLooper("abc");
+abc(); // should return 'a' on this first call
+abc(); // should return 'b' on this second call
+abc(); // should return 'c' on this third call
+abc(); // should return 'a' again on this fourth call
+*/
+
+function makeLooper(str) {
+  let actualElement = 0;
+  return () =>{
+    return str[actualElement++ % str.length];
+  }
+}
