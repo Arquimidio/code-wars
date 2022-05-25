51bac5a16052b0aedefb9621dcbb47768aee83cb
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-25 14:15:31 -0300

    Add "Can you keep a secret?"

---
+++ canYouKeepASecret.js
@@ -0,0 +1,14 @@
+/*
+
+*/
+
+function createSecretHolder(secret) {
+  return ({
+    getSecret: function(){
+      return secret;
+    },
+    setSecret: function(newSecret){
+      secret = newSecret;
+    }
+  })
+}
