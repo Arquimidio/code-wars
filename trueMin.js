9f6bbc8c690584aa3241476b5c13ddc8c6cc0bf5
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-05-28 11:18:30 -0300

    Add "True Min"

---
+++ trueMin.js
@@ -0,0 +1,17 @@
+/*
+The Math.min function has stopped working, so we have to use our own function. We are off to a good start, but this function doesn't seem to handle everything properly. Add in the proper checks to return NaN for anything that isn't an actual number, except treat null like 0.
+
+Note: This min function need not handle more than two arguments.
+
+function min(a, b){
+  return (a<b)?a:b;
+}
+*/
+
+function min(...args){
+  return args.reduce((acc, cur) => {
+    if(typeof cur === 'undefined' || Number.isNaN(cur)) return NaN;
+    cur = Number(cur);
+    return cur < acc? cur : acc;
+  }, Number(args[0]));
+}
