4baa73b89c8c23ce02d1b75c44a833d722623fab
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-06-01 06:33:50 -0300

    Add "Sort Reloaded"

---
+++ sortReloaded.js
@@ -0,0 +1,25 @@
+/*
+Array.prototype.sort() is a pretty handy feature of the JS core, but when it comes to order an array of numbers, sometimes it can pollute our code.
+
+Task
+In this Kata you have to extend the Array object and add the sortReloaded(dir) method to it in order to make this task easier and cleaner.
+
+It should receive a dir parameter which has with two possible values 'asc' or 'desc', and return a new array ordered ascendingly or descendingly respectively. If no dir parameter is set it should assume 'asc' by default. If it has an invalid value, return false.
+
+Note
+For the effects of this Kata, all the arrays will contain only Integer numbers so you don't have to care about validating them.
+
+You have to return a new array, do not modify the original array.
+*/
+
+Array.prototype.sortReloaded = function(dir='asc'){
+  const newArr = [...this];
+  switch(dir){
+      case 'asc':
+        return newArr.sort((a, b) => a - b);
+      case 'desc':
+        return newArr.sort((a, b) => b - a);
+      default:
+        return false;
+  }
+}
