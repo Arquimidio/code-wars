4573096e735d580684975deb7979b16b202b5044
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-06-08 18:57:35 -0300

    Add "Leaderboard Climbers"

---
+++ leaderboardClimbers.js
@@ -0,0 +1,51 @@
+/*
+Leaderboard climbers
+In this kata you will be given a leaderboard of unique names for example:
+
+['John',
+ 'Brian',
+ 'Jim',
+ 'Dave',
+ 'Fred']
+Then you will be given a list of strings for example:
+
+['Dave +1', 'Fred +4', 'Brian -1']
+Then you sort the leaderboard.
+
+The steps for our example would be:
+
+# Dave up 1
+['John',
+ 'Brian',
+ 'Dave',
+ 'Jim',
+ 'Fred']
+# Fred up 4
+['Fred',
+ 'John',
+ 'Brian',
+ 'Dave',
+ 'Jim']
+# Brian down 1
+['Fred',
+ 'John',
+ 'Dave',
+ 'Brian',
+ 'Jim']
+Then once you have done this you need to return the leaderboard.
+
+All inputs will be valid. All strings in the second list will never ask to move a name up higher or lower than possible eg. "John +3" could not be added to the end of the second input list in the example above.
+
+The strings in the second list will always be something in the leaderboard followed by a space and a + or - sign followed by a number.
+*/
+
+function leaderboardSort(leaderboard, changes) {
+  for(let i = 0; i < changes.length; i++){
+    const [name, pos] = changes[i].split(' ');
+    const changeCount = Number(pos.slice(1));
+    const index = leaderboard.indexOf(name);
+    leaderboard.splice(index, 1);
+    leaderboard.splice(index - pos, 0, name);
+  }
+  return leaderboard;
+}
