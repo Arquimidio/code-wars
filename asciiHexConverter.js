d5cd4a162b79e1d366e90094718c443dfa5c11c8
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-06-15 05:57:30 -0300

    Cleaned ASCII HEX Converter instructions

--- asciiHexConverter.js
+++ asciiHexConverter.js
@@ -6,7 +6,6 @@ Converter.toHex("Look mom, no hands")
 => "4c6f6f6b206d6f6d2c206e6f2068616e6473"
 
 Converter.toAscii("4c6f6f6b206d6f6d2c206e6f2068616e6473")
-=> "Look mom, no hands"
 */
 
 var Converter = {
