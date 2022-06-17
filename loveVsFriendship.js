b4f14ef204825b9e3cb11c90344153c29f30834b
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-06-16 22:03:47 -0300

    Add "Love vs Friendship"

---
+++ loveVsFriendship.js
@@ -0,0 +1,15 @@
+/*
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice stronger than love :-)
+
+The input will always be in lowercase and never be empty.
+*/
+
+function wordsToMarks(string){
+  return [...string].reduce((acc, cur) => acc + cur.charCodeAt() - 96, 0);
+}
