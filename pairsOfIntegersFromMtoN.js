75c32c9c73bf51cc5d1677e9312f35d9bed1dec6
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-06-17 21:53:43 -0300

    Add "Pairs of Integers from M to N"

---
+++ pairsOfIntegersFromMtoN.js
@@ -0,0 +1,22 @@
+/*
+Implement a function that receives two integers m and n and generates a sorted list of pairs (a, b) such that m <= a <= b <= n.
+
+The input m will always be smaller than or equal to n (e.g., m <= n)
+
+Example
+m = 2
+n = 4
+
+result = [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)]
+*/
+
+function generatePairs(m, n) {
+  const arr = [];
+  while(m <= n){
+    for(let i = m; i <= n; i++){
+      arr.push([m, i])
+    }
+    m++;
+  }
+  return arr;
+}
