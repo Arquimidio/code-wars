c7de36e86db099cbe30d382c3f9fd19e611764ee
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-06-21 21:30:17 -0300

    Add "Merge Overlapping Strings"

---
+++ mergeOverlappingStr.js
@@ -0,0 +1,21 @@
+/*
+This kata requires you to write a function which merges two strings together. It does so by merging the end of the first string with the start of the second string together when they are an exact match.
+
+"abcde" + "cdefgh" => "abcdefgh"
+"abaab" + "aabab" => "abaabab"
+"abc" + "def" => "abcdef"
+"abc" + "abc" => "abc"
+NOTE: The algorithm should always use the longest possible overlap.
+
+"abaabaab" + "aabaabab" would be "abaabaabab" and not "abaabaabaabab"
+*/
+
+function mergeStrings(first, second){
+  for(let i = 0; i < first.length; i++){
+    const firstSlice = first.slice(i);
+    if(second.startsWith(firstSlice)){
+      return first.slice(0, i) + second;
+    }
+  }
+  return first + second;
+}
